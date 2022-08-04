bc1a71bbe217e45412fbc014714378e1e163936a
Bobby Hinton <bob.g.hint@gmail.com>
2022-08-04 11:56:09 -0500

    commit tree f09e45fb23f7d5fdd34ccba592ae914992802c18

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // timeRemaining is currently set to 10 seconds 
+  // need some logic to figure out if our timeRemaining is at 0 or not
+  // when at  0 we need to setTimeRemaining to 10 and provide onAnswered with
+  // a false argument 
+
+  useEffect(() => {
+    const timer = setTimeout(()=> {
+      if(timeRemaining === 0){
+        setTimeRemaining(10)
+        onAnswered(false)
+      } else {
+       setTimeRemaining(timeRemaining => timeRemaining - 1)
+      }
+    }, 1000)
+
+    return function cleanup() {
+      clearTimeout(timer)
+    }
+  },[timeRemaining])
 
   // add useEffect code
 
